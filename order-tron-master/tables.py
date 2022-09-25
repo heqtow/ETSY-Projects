5c4f37abc6fb8bdaf32ea751e66b52d7499fd004
heqtor <100768796+heqtow@users.noreply.github.com>
2022-09-25 23:29:51 +0300

    Add files via upload

--- order-tron-master/tables.py
+++ order-tron-master/tables.py
@@ -5,26 +5,26 @@ from datetime import datetime, timezone
 @TableMaker.create([
   #("Order Number", "orderNumber"),
   ("Kargo tarihi", "creationTimestamp"),
-  #Kargo no
+  ("Kargo no", "cargoNo"),
   ("ALICI / MÜŞTERİ ADI", "buyerName"),
   ("Ülke", "countryId"),
-  #İhracat Tipi
-  #Kargo firması
-  #Bayi Satıcı Kodu
-  #Fatura No
+  ("İhracat Tipi", "ihracatTipi"),#İhracat Tipi
+  ("Kargo firması", "kargoFirmasi"),#Kargo firması
+  ("Bayi Satıcı Kodu", "bayiSaticiKodu"),#Bayi Satıcı Kodu
+  ("Fatura No", "faturaNo"),#Fatura No
   ("FATURA BEDELİ", "amountPaid"),
-  #Para Birimi
-  #Kur
-  #Ürün
-  #phone
+  ("Para Birimi", "paraBirimi"),#Para Birimi
+  ("Kur", "kur"),#Kur
+  ("Ürün", "ürün"),#Ürün
+  ("phone", "phone"),#phone
   ("Address 1", "address1"),
   ("Address 2", "address2"),
   ("Şehir", "city"),
   ("Eyalet", "state"),
   ("Posta kodu", "zip"),
-  ("Email", "buyerEmail")
-  #IOSS
-  #Boyut
+  ("Email", "buyerEmail"),
+  ("IOSS", "ioss"),#IOSS
+  ("Boyut", "boyut")#Boyut
 ])
 def order_overview(add_row, session, page):
   receipts = session.receipts(page)
@@ -50,24 +50,24 @@ def order_overview(add_row, session, page):
 #        receipt['receipt_id']
 #      ),
       tsz_to_meta_str(receipt["creation_tsz"]),
-      #Kargo no
+      receipt["name"],  #Kargo no
       receipt["name"],
       receipt["country_id"],
-      #İhracat Tipi
-      #Kargo firması
-      #Bayi Satıcı Kodu
-      #Fatura No
+      receipt["name"],  #İhracat Tipi
+      receipt["name"],  #Kargo firması
+      receipt["name"],  #Bayi Satıcı Kodu
+      receipt["name"],  #Fatura No
       MetaStr(f"{receipt['subtotal']}", float(receipt['subtotal'])),
-      #Para Birimi
-      #Kur
-      #Ürün
-      #phone
+      receipt["name"],  #Para Birimi
+      receipt["name"],  #Kur
+      receipt["name"],  #Ürün
+      receipt["name"],  #phone
       receipt["first_line"],
       receipt["second_line"],
       receipt["city"],
       receipt["state"],
       receipt["zip"],
-      receipt["buyer_email"]
-      #IOSS
-      #Boyut
+      receipt["buyer_email"],
+      receipt["name"],  #IOSS
+      receipt["name"]  #Boyut
     )
